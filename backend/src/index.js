7e68edc8fbd0c306712d707cc00de58f601f916f
Sergio Diaz Toro <sdtoro@outlook.com>
2025-02-28 01:09:18 -0500

    Se realizan algunos cambios para pubicar en nube

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import app from './app.js';
 
 const PORT = app.get('port');
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, "0.0.0.0", () => {
     console.log(`Servidor funcionando en: http://localhost:${PORT}`);
 });
 
